7fb06ebf6293899523a23c28bb4ebff85d6eb205
Vyacheslav Bezborodov <vyacheslav.bezborodov@gmail.com>
2018-02-03 11:58:25 +0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+setup(
+    name='chats',
+    version='0.0.1',
+    author='Vyacheslav Bezborodov',
+    author_email='vyacheslav.bezborodov@gmail.com',
+    description='chats',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    entry_points={
+        'console_scripts': [
+            'chats=chats.cli:cli',
+        ],
+    },
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    extras_require={
+        'test': [
+            'tox==2.9.1',
+            'pytest==3.4.0',
+            'pytest-asyncio==0.8.0',
+        ],
+    },
+)
